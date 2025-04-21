d0a952601a97619bc18023cf691443cafea9e63b
yehan2002 <yehanjaya2002@gmail.com>
2025-04-21 22:03:57 +0530

    Add user repo.

---
+++ order-service/repo/order.go
@@ -0,0 +1,280 @@
+package repo
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/SE-WE-22-Projects/DS-Food-Delivery/order-service/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var ErrNoOrder = fmt.Errorf("order not found")
+var ErrStateChange = fmt.Errorf("invalid order state change")
+var ErrCannotCancelOrder = fmt.Errorf("cannot cancel order")
+
+type TransactionId = string
+
+type OrderRepo interface {
+	// CreateOrderFromCart creates a order from the users current cart content.
+	CreateOrderFromCart(ctx context.Context, userId UserId) (bson.ObjectID, error)
+	// CreateOrder creates a new order. (used for tests)
+	CreateOrder(ctx context.Context, order *models.Order) (bson.ObjectID, error)
+	// GetOrderById returns the order with the given id
+	GetOrderById(ctx context.Context, orderId bson.ObjectID) (*models.Order, error)
+	// CancelOrder cancels the given order.
+	// This method can only be used before the order is accepted by the restaurant.
+	CancelOrder(ctx context.Context, orderId bson.ObjectID) error
+	// UpdatePaymentStatus updates the payment status of the order
+	UpdatePaymentStatus(ctx context.Context, orderId bson.ObjectID, successful bool, transactionId TransactionId) error
+	// UpdateAcceptedStatus updates the accepted status.
+	UpdateAcceptedStatus(ctx context.Context, orderId bson.ObjectID, accepted bool, cancelReason string) error
+	// SetOrderPickupReady marks the order as ready to pickup
+	SetOrderPickupReady(ctx context.Context, orderId bson.ObjectID) error
+	// SetDeliveryDriver sets the delivery driver that will deliver the order.
+	SetDeliveryDriver(ctx context.Context, orderId bson.ObjectID, driverId UserId) error
+	// SetOrderDelivered marks the order as delivered
+	SetOrderDelivered(ctx context.Context, orderId bson.ObjectID) error
+}
+
+type orderRepo struct {
+	orders *mongo.Collection
+	client *mongo.Client
+	cart   CartRepo
+}
+
+// CreateOrderFromCart creates a order from the users current cart content.
+func (o *orderRepo) CreateOrderFromCart(ctx context.Context, userId UserId) (bson.ObjectID, error) {
+	session, err := o.client.StartSession()
+	if err != nil {
+		return bson.NilObjectID, err
+	}
+	defer session.EndSession(ctx)
+
+	orderId, err := session.WithTransaction(ctx, func(ctx context.Context) (any, error) {
+		cart, err := o.cart.GetCartByUserId(ctx, userId)
+		if err != nil {
+			return nil, err
+		}
+
+		// convert cart items to [models.OrderItem]
+		orderItems := make([]models.OrderItem, len(cart.Items))
+		for i, item := range cart.Items {
+			orderItems[i] = models.OrderItem{
+				ItemId: item.ItemId,
+				Name:   item.Name,
+				Amount: item.Amount,
+				Extra:  item.Extra,
+				Price:  item.Price,
+			}
+		}
+
+		// create the order
+		result, err := o.orders.InsertOne(ctx, models.Order{
+			UserId: userId,
+			Items:  orderItems,
+			Coupon: cart.Coupon,
+			Price:  cart.TotalPrice,
+			Status: models.StatusPaymentPending,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		// clear the user's cart.
+		err = o.cart.ClearCart(ctx, userId)
+		if err != nil {
+			return nil, err
+		}
+
+		return result.InsertedID, nil
+	})
+
+	if err != nil {
+		return bson.NilObjectID, err
+	}
+
+	return orderId.(bson.ObjectID), nil
+}
+
+// CreateOrder creates a new order. (used for tests)
+func (o *orderRepo) CreateOrder(ctx context.Context, order *models.Order) (bson.ObjectID, error) {
+	result, err := o.orders.InsertOne(ctx, order)
+	if err != nil {
+		return bson.NilObjectID, err
+	}
+	return result.InsertedID.(bson.ObjectID), nil
+}
+
+// GetOrderById returns the order with the given id
+func (o *orderRepo) GetOrderById(ctx context.Context, orderId bson.ObjectID) (*models.Order, error) {
+	var order models.Order
+
+	var tmp bson.M
+	err := o.orders.FindOne(ctx, bson.D{{Key: "_id", Value: orderId}}).Decode(&tmp)
+	if err == nil {
+		fmt.Println(tmp)
+	}
+
+	err = o.orders.FindOne(ctx, bson.D{{Key: "_id", Value: orderId}}).Decode(&order)
+	if err != nil {
+		return nil, err
+	}
+	return &order, nil
+}
+
+// SetDeliveryDriver sets the delivery driver that will deliver the order.
+func (o *orderRepo) SetDeliveryDriver(ctx context.Context, orderId bson.ObjectID, driverId UserId) error {
+	res, err := o.orders.UpdateByID(ctx, orderId, bson.A{bson.M{
+		"$set": bson.D{
+			updateIfStatus("status", models.StatusDelivering, models.StatusAwaitingPickup),
+			updateIfStatus("driver", driverId, models.StatusAwaitingPickup),
+		},
+	}})
+	if err != nil {
+		return err
+	}
+
+	if res.MatchedCount == 0 {
+		// Order not found
+		return ErrNoOrder
+	} else if res.ModifiedCount == 0 {
+		// If modified count is 0, the order was found but its status was not [StatusAwaitingPickup].
+		return ErrStateChange
+	}
+
+	return nil
+}
+
+// UpdateAcceptedStatus updates the accepted status.
+func (o *orderRepo) UpdateAcceptedStatus(ctx context.Context, orderId bson.ObjectID, accepted bool, cancelReason string) error {
+	newState := models.StatusPreparing
+	if !accepted {
+		newState = models.StatusRejected
+	}
+
+	res, err := o.orders.UpdateByID(ctx, orderId, bson.A{bson.M{
+		"$set": bson.D{
+			updateIfStatus("status", newState, models.StatusPendingAccept),
+			updateIfStatus("res_rej_reason", cancelReason, models.StatusPendingAccept),
+		},
+	}})
+	if err != nil {
+		return err
+	}
+
+	if res.MatchedCount == 0 {
+		// Order not found
+		return ErrNoOrder
+	} else if res.ModifiedCount == 0 {
+		// If modified count is 0, the order was found but its status was not [StatusPaymentPending].
+		return ErrStateChange
+	}
+
+	return nil
+}
+
+// SetOrderPickupReady marks the order as ready to pickup
+func (o *orderRepo) SetOrderPickupReady(ctx context.Context, orderId bson.ObjectID) error {
+	res, err := o.orders.UpdateByID(ctx, orderId, bson.A{bson.M{
+		"$set": bson.D{
+			updateIfStatus("status", models.StatusAwaitingPickup, models.StatusPreparing),
+		},
+	}})
+	if err != nil {
+		return err
+	}
+
+	if res.MatchedCount == 0 {
+		// Order not found
+		return ErrNoOrder
+	} else if res.ModifiedCount == 0 {
+		// If modified count is 0, the order was found but its status was not [StatusPaymentPreparing].
+		return ErrStateChange
+	}
+
+	return nil
+}
+
+// UpdateDeliveryStatus updates the delivery status of the order.
+func (o *orderRepo) SetOrderDelivered(ctx context.Context, orderId bson.ObjectID) error {
+	res, err := o.orders.UpdateByID(ctx, orderId, bson.A{bson.M{
+		"$set": bson.D{
+			updateIfStatus("status", models.StatusDelivered, models.StatusDelivering),
+		},
+	}})
+	if err != nil {
+		return err
+	}
+
+	if res.MatchedCount == 0 {
+		// Order not found
+		return ErrNoOrder
+	} else if res.ModifiedCount == 0 {
+		// If modified count is 0, the order was found but its status was not [StatusDelivering].
+		return ErrStateChange
+	}
+
+	return nil
+}
+
+// UpdatePaymentStatus updates the payment status of the order
+func (o *orderRepo) UpdatePaymentStatus(ctx context.Context, orderId bson.ObjectID, successful bool, transactionId TransactionId) error {
+	newState := models.StatusPendingAccept
+	if !successful {
+		newState = models.StatusPaymentFailed
+	}
+
+	res, err := o.orders.UpdateByID(ctx, orderId, bson.A{bson.M{
+		"$set": bson.D{
+			updateIfStatus("status", newState, models.StatusPaymentPending),
+			updateIfStatus("transaction_id", transactionId, models.StatusPaymentPending),
+		},
+	}})
+	if err != nil {
+		return err
+	}
+
+	if res.MatchedCount == 0 {
+		// Order not found
+		return ErrNoOrder
+	} else if res.ModifiedCount == 0 {
+		// If modified count is 0, the order was found but its status was not [StatusPaymentPending].
+		return ErrStateChange
+	}
+
+	return nil
+}
+
+var canCancelStatus = []models.OrderStatus{models.StatusPaymentPending, models.StatusPendingAccept, models.StatusPaymentFailed, models.StatusCanceled}
+
+// CancelOrder cancels the given order.
+// This method can only be used before the order is accepted by the restaurant.
+func (o *orderRepo) CancelOrder(ctx context.Context, orderId bson.ObjectID) error {
+	res, err := o.orders.UpdateByID(ctx, orderId, bson.A{bson.M{
+		"$set": bson.D{
+			updateIfStatus("status", models.StatusCanceled, canCancelStatus...),
+		},
+	}})
+	if err != nil {
+		return err
+	}
+
+	if res.MatchedCount == 0 {
+		// Order not found
+		return ErrNoOrder
+	} else if res.ModifiedCount == 0 {
+		// If modified count is 0, the order was found but its status was not in a cancellable state.
+		return ErrCannotCancelOrder
+	}
+
+	return nil
+}
+
+func NewOrderRepo(db *mongo.Database, cartRepo CartRepo) (OrderRepo, error) {
+	return &orderRepo{
+		orders: db.Collection("orders"),
+		cart:   cartRepo,
+		client: db.Client(),
+	}, nil
+}
---
+++ order-service/repo/order_test.go
@@ -0,0 +1,244 @@
+package repo
+
+import (
+	"context"
+	"testing"
+
+	"github.com/SE-WE-22-Projects/DS-Food-Delivery/order-service/models"
+	"github.com/SE-WE-22-Projects/DS-Food-Delivery/shared/database"
+	"github.com/yehan2002/is/v2"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type orderTest struct{}
+
+func TestOrder(t *testing.T) {
+	is.Suite(t, &orderTest{})
+}
+
+func (o *orderTest) TestCreateFromCart(is is.Is) {
+	db, close := database.ConnectTestDB()
+	defer close()
+
+	itemId := bson.NewObjectID().Hex()
+	userId := bson.NewObjectID().Hex()
+	couponId := bson.NewObjectID().Hex()
+	itemData := map[string]any{"a": int32(1)}
+
+	// setup cart
+	cartRepo, err := NewCartRepo(db, NewItemRepo(), NewPromoRepo())
+	is.Ok(err, "failed to create cart repo")
+	_, err = cartRepo.AddItem(context.TODO(), userId, itemId, 10, itemData)
+	is.Ok(err, "failed to add item")
+	cart, err := cartRepo.SetCartCoupon(context.TODO(), userId, couponId)
+	is.Ok(err, "failed to apply coupon")
+
+	repo, err := NewOrderRepo(db, cartRepo)
+	is.Ok(err, "failed to create repo")
+
+	orderId, err := repo.CreateOrderFromCart(context.TODO(), userId)
+	is.Ok(err, "failed to create order")
+	is(orderId != bson.NilObjectID, "invalid obj id")
+
+	order, err := repo.GetOrderById(context.TODO(), orderId)
+	is.Ok(err, "failed to get order")
+
+	is(len(order.Items) == len(cart.Items), "invalid item amount")
+	is(order.Items[0].ItemId == cart.Items[0].ItemId, "invalid item data")
+	is(order.Items[0].Amount == cart.Items[0].Amount, "invalid item data")
+	is(order.Items[0].Price == cart.Items[0].Price, "invalid item data")
+	is(order.Items[0].Name == cart.Items[0].Name, "invalid item data")
+	is.Equal(order.Items[0].Extra, cart.Items[0].Extra, "invalid item data")
+
+	is(order.Coupon != nil, "coupon was not set")
+	is.Equal(order.Coupon.CouponId, couponId, "incorrect coupon id")
+}
+
+func (o *orderTest) TestOrderPaymentSuccess(is is.Is) {
+	db, close := database.ConnectTestDB()
+	defer close()
+
+	cartRepo, err := NewCartRepo(db, NewItemRepo(), NewPromoRepo())
+	is.Ok(err, "failed to create cart repo")
+	repo, err := NewOrderRepo(db, cartRepo)
+	is.Ok(err, "failed to create repo")
+
+	orderId, err := repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusPaymentPending,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.UpdatePaymentStatus(context.TODO(), orderId, true, "abc123")
+	is.Ok(err, "Failed to update status")
+
+	order, err := repo.GetOrderById(context.TODO(), orderId)
+	is.Ok(err, "Failed to get order")
+
+	is(order.Status == models.StatusPendingAccept, "incorrect status")
+	is(order.TransactionId == "abc123", "incorrect transaction id")
+
+	err = repo.UpdatePaymentStatus(context.TODO(), orderId, true, "abc123")
+	is.Err(err, ErrStateChange, "should fail to update payment status multiple times")
+}
+
+func (o *orderTest) TestOrderCancel(is is.Is) {
+	db, close := database.ConnectTestDB()
+	defer close()
+
+	// setup repos
+	cartRepo, err := NewCartRepo(db, NewItemRepo(), NewPromoRepo())
+	is.Ok(err, "failed to create cart repo")
+	repo, err := NewOrderRepo(db, cartRepo)
+	is.Ok(err, "failed to create repo")
+
+	// create order
+	orderId, err := repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusPaymentPending,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.CancelOrder(context.Background(), orderId)
+	is.Ok(err, "Failed to cancel order")
+
+	orderId, err = repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusPreparing,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.CancelOrder(context.Background(), orderId)
+	is.Err(err, ErrCannotCancelOrder, "should not allow canceling order")
+}
+
+func (o *orderTest) TestOrderAccept(is is.Is) {
+	db, close := database.ConnectTestDB()
+	defer close()
+
+	// setup repos
+	cartRepo, err := NewCartRepo(db, NewItemRepo(), NewPromoRepo())
+	is.Ok(err, "failed to create cart repo")
+	repo, err := NewOrderRepo(db, cartRepo)
+	is.Ok(err, "failed to create repo")
+
+	// create order
+	orderId, err := repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusPendingAccept,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.UpdateAcceptedStatus(context.Background(), orderId, true, "")
+	is.Ok(err, "Failed to accept order")
+
+	orderId, err = repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusCanceled,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.UpdateAcceptedStatus(context.Background(), orderId, false, "")
+	is.Err(err, ErrStateChange, "should not allow changing status order")
+}
+
+func (o *orderTest) TestOrderSetDriver(is is.Is) {
+	db, close := database.ConnectTestDB()
+	defer close()
+
+	// setup repos
+	cartRepo, err := NewCartRepo(db, NewItemRepo(), NewPromoRepo())
+	is.Ok(err, "failed to create cart repo")
+	repo, err := NewOrderRepo(db, cartRepo)
+	is.Ok(err, "failed to create repo")
+
+	// create order
+	orderId, err := repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusAwaitingPickup,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.SetDeliveryDriver(context.Background(), orderId, "driver-id")
+	is.Ok(err, "Failed to set driver")
+
+	orderId, err = repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusCanceled,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.SetDeliveryDriver(context.Background(), orderId, "driver-id")
+	is.Err(err, ErrStateChange, "should not allow changing status order")
+}
+
+func (o *orderTest) TestOrderSetDeliveryComplete(is is.Is) {
+	db, close := database.ConnectTestDB()
+	defer close()
+
+	// setup repos
+	cartRepo, err := NewCartRepo(db, NewItemRepo(), NewPromoRepo())
+	is.Ok(err, "failed to create cart repo")
+	repo, err := NewOrderRepo(db, cartRepo)
+	is.Ok(err, "failed to create repo")
+
+	// create order
+	orderId, err := repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusDelivering,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.SetOrderDelivered(context.Background(), orderId)
+	is.Ok(err, "Failed to set driver")
+
+	orderId, err = repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusCanceled,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.SetOrderDelivered(context.Background(), orderId)
+	is.Err(err, ErrStateChange, "should not allow changing status order")
+}
+
+func (o *orderTest) TestOrderPrepareDone(is is.Is) {
+	db, close := database.ConnectTestDB()
+	defer close()
+
+	// setup repos
+	cartRepo, err := NewCartRepo(db, NewItemRepo(), NewPromoRepo())
+	is.Ok(err, "failed to create cart repo")
+	repo, err := NewOrderRepo(db, cartRepo)
+	is.Ok(err, "failed to create repo")
+
+	// create order
+	orderId, err := repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusPreparing,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.SetOrderPickupReady(context.Background(), orderId)
+	is.Ok(err, "Failed to set pickup ready")
+
+	orderId, err = repo.CreateOrder(context.TODO(), &models.Order{
+		UserId: "12314124",
+		Price:  100,
+		Status: models.StatusCanceled,
+	})
+	is.Ok(err, "Failed to create order")
+
+	err = repo.SetOrderPickupReady(context.Background(), orderId)
+	is.Err(err, ErrStateChange, "should not allow changing status order")
+}
