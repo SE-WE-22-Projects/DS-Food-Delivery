e67286abda0fe988bb11bef64f58063cdfbfbf5b
navindunimsara2001 <navidunimsara@gmail.com>
2025-04-26 13:50:01 +0530

    api call functions initiated

---
+++ admin-ui/src/api/menu.ts
@@ -0,0 +1,31 @@
+export interface MenuType {
+
+}
+
+export const getAllMenuItems = async () => {
+
+}
+
+export const getRestaurantMenuItems = async () => {
+
+}
+
+export const getMenuItemById = async () => {
+
+}
+
+export const createMenuItem = async () => {
+
+}
+
+export const updateMenuItemById = async () => {
+
+}
+
+export const deleteMenuItemById = async () => {
+
+}
+
+const uploadMenuItemImage = async () => {
+
+}
---
+++ admin-ui/src/api/restaurant.ts
@@ -0,0 +1,31 @@
+export interface RestaurantType {
+
+}
+
+export const getAllRestaurants = async () => {
+
+}
+
+export const getRestaurantById = async () => {
+
+}
+
+export const createRestaurant = async () => {
+
+}
+
+export const updateRestaurantById = async () => {
+
+}
+
+export const deleteRestaurantById = async () => {
+
+}
+
+const uploadLogo = async () => {
+
+}
+
+const uploadCover = async() => {
+    
+}
