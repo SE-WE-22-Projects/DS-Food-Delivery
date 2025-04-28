2ab977ec8a013fc2cdddf3d1deb629b63b5eca70
navindunimsara2001 <navidunimsara@gmail.com>
2025-04-28 17:18:44 +0530

    approve functionality for pending restaurants

--- admin-ui/src/components/restaurant/PendingRestaurantTab.tsx
+++ admin-ui/src/components/restaurant/PendingRestaurantTab.tsx
@@ -1,11 +1,30 @@
 import api from '@/api';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import React from 'react'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
+import { Button } from '../ui/button';
+import { BadgeInfo, Eye } from 'lucide-react';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const PendingRestaurantTab = () => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const query = useQuery({ queryKey: ['restaurants', 'pending'], queryFn: api.restaurant.getAllPendingRestaurants });
+
+  const approveRestaurant = useMutation({
+    mutationFn: (restaurantId: string) => api.restaurant.approveRestaurantById(restaurantId, true),
+    onSuccess: () => { queryClient.invalidateQueries({ queryKey: ['restaurants'] }) }
+  })
+
+  const handleApproveRestaurant = async (restaurantId: string) => {
+    try {
+      await approveRestaurant.mutateAsync(restaurantId);
+      toast.success("Successfully approved restaurant.");
+    } catch (error) {
+      toast.error("Failed to approve restaurant.");
+      console.error(error);
+    }
+  }
 
   return (
     <>
@@ -30,7 +49,18 @@ const PendingRestaurantTab = () => {
                   <TableCell>{`${restaurant.address.no},${restaurant.address.street},${restaurant.address.town},${restaurant.address.city}`}</TableCell>
                   <TableCell>{restaurant.address.postal_code}</TableCell>
                   <TableCell>{restaurant.registration_no}</TableCell>
-                  <TableCell>{ }</TableCell>
+                  <TableCell className='flex gap-3'>
+                    <Button className='bg-orange-400 hover:bg-orange-500 hover:scale-[1.03]'
+                      onClick={() => { navigate(`/dashboard/restaurant/${restaurant.id}`) }}
+                    >
+                      View <Eye />
+                    </Button>
+                    <Button className='bg-green-400 hover:bg-green-500 hover:scale-[1.03]'
+                      onClick={() => { handleApproveRestaurant(restaurant.id) }}
+                    >
+                      Approve <BadgeInfo />
+                    </Button>
+                  </TableCell>
                 </TableRow>
               )
             })
