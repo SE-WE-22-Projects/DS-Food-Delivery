e439865abc748ac7d62b5aeb133be4d05ea56280
navindunimsara2001 <navidunimsara@gmail.com>
2025-04-29 19:44:58 +0530

    menu part

--- web-ui/src/components/menu/MenuItem.tsx
+++ web-ui/src/components/menu/MenuItem.tsx
@@ -3,14 +3,18 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from '../ui/button';
 import { useCartDialog } from '../cart/CartDialog';
 import getImageUrl from '@/lib/images';
+import { useNavigate } from 'react-router-dom';
 
 
 const MenuItem = ({ item }: { item: MenuItemType }) => {
+    const navigate = useNavigate()
     const cartDialog = useCartDialog();
 
     return (
         <>
-            <Card className='mx-2 my-2 w-2xs pt-0'>
+            <Card className='mx-2 my-2 w-2xs pt-0'
+            onDoubleClick={()=>navigate(`/menu/${item.id}`)}
+            >
                 <CardHeader className='px-0 pt-0'>
                     <CardDescription className='w-2xs h-[160px] bg-cover bg-no-repeat rounded-t-xl' style={{ backgroundImage: `url(${getImageUrl(item.image)})` }} />
                     <CardTitle className='px-2'>{item.name}</CardTitle>
