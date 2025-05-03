726305df50dbcb8383447f2eaf3360df7bc20077
navindunimsara2001 <navidunimsara@gmail.com>
2025-05-03 14:36:06 +0530

    formatting

--- shared/notify/notify.go
+++ shared/notify/notify.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Config struct {
-	Url       string
-	Topic     string
+	Url   string
+	Topic string
 }
 
 type Notify struct {
@@ -22,15 +22,15 @@ func (k *Notify) Connect(ctx context.Context, cfg Config) error {
 		return err
 	}
 	k.client = conn
-	return nil;
+	return nil
 }
 
-func (k *Notify) Send(ctx context.Context, message Message) error{
+func (k *Notify) Send(ctx context.Context, message Message) error {
 	buf, err := json.Marshal(message)
 	if err != nil {
 		return err
 	}
-	_,err = k.client.WriteMessages(kafka.Message{Value: buf})
+	_, err = k.client.WriteMessages(kafka.Message{Value: buf})
 	if err != nil {
 		return err
 	}
