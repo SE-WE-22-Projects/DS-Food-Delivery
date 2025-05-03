4d33d2c9a50b5d330eef301f5a3df5b5cdcfe307
navindunimsara2001 <navidunimsara@gmail.com>
2025-05-03 14:35:15 +0530

    shared: kafka config

--- shared/go.mod
+++ shared/go.mod
@@ -30,7 +30,9 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/segmentio/kafka-go v0.4.47 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
---
+++ shared/notify/notify.go
@@ -0,0 +1,38 @@
+package notify
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Config struct {
+	Url       string
+	Topic     string
+}
+
+type Notify struct {
+	client *kafka.Conn
+}
+
+func (k *Notify) Connect(ctx context.Context, cfg Config) error {
+	conn, err := kafka.DialLeader(ctx, "tcp", cfg.Url, cfg.Topic, 0)
+	if err != nil {
+		return err
+	}
+	k.client = conn
+	return nil;
+}
+
+func (k *Notify) Send(ctx context.Context, message Message) error{
+	buf, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	_,err = k.client.WriteMessages(kafka.Message{Value: buf})
+	if err != nil {
+		return err
+	}
+	return nil
+}
