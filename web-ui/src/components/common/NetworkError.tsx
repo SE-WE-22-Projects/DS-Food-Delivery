cc4854807d642ef5a7ef340168c894b691370bc5
yehan2002 <yehanjaya2002@gmail.com>
2025-05-06 00:08:51 +0530

    web-ui: Improve network error component ui.

--- web-ui/src/components/common/NetworkError.tsx
+++ web-ui/src/components/common/NetworkError.tsx
@@ -5,13 +5,21 @@ import { Button } from '../ui/button';
 
 const NetworkError = ({ what }: { what?: string }) => {
     const { resetBoundary } = useErrorBoundary();
-    return <Alert variant="destructive" className="max-w-2xl mx-auto">
-        <AlertTriangle className="h-4 w-4" />
-        <AlertTitle>Failed to Load {what ?? "Page"}</AlertTitle>
-        <AlertDescription>
-            Could not fetch data from the server due to a network error.
-            Please check your connection and try again.
-            <Button onClick={resetBoundary}>Try Again</Button>
+    return <Alert variant="destructive" className="max-w-lg mx-auto">
+        <AlertTitle className='flex flex-col mx-auto'>
+            <AlertTriangle className="h-16 w-21 mx-auto mb-2" />
+            <div className='text-lg'>
+                Failed to Load {what ?? "Content"}
+            </div>
+        </AlertTitle>
+        <AlertDescription className='flex flex-col mx-auto mt-4'>
+            <div>
+                Could not fetch data from the server due to a network error.
+            </div>
+            <div>
+                Please check your connection and try again.
+            </div>
+            <Button className='mt-4 mb-4' onClick={resetBoundary}>Try Again</Button>
         </AlertDescription>
     </Alert>
 }
