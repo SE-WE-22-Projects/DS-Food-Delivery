62a3c3daf0d7697175e7a85bbb714426d81800dd
yehan2002 <yehanjaya2002@gmail.com>
2025-05-06 15:33:34 +0530

    web-ui: add not found text to default image.

--- web-ui/src/lib/images.ts
+++ web-ui/src/lib/images.ts
@@ -3,7 +3,7 @@ const getImageUrl = (image: string | undefined, size: { width: number, height: n
     if (image && image.startsWith("/api/v1/uploads/")) {
         return image
     }
-    return `https://placehold.co/${size.width}X${size.height}/FFFFFF/000000/png`
+    return `https://placehold.co/${size.width}X${size.height}/bcbcbc/000000/png?text=Image Not Found`
 }
 
 export default getImageUrl;
