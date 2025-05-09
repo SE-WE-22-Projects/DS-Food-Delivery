276eb7e3c6ae1f88ec2f3abde07ebf84a8431e4d
yehan2002 <yehanjaya2002@gmail.com>
2025-05-09 12:32:33 +0530

    shared: location: code cleanup.

--- shared/location/location.go
+++ shared/location/location.go
@@ -3,50 +3,69 @@ package location
 import (
 	"context"
 	"errors"
+	"sync"
 
 	"googlemaps.github.io/maps"
 )
 
 var ErrLocation = errors.New("unable to get location")
 
-type LocationService struct {
-	client *maps.Client
-	cache  map[string]*maps.LatLng
+type LatLng struct {
+	maps.LatLng
+	Exact bool
+	Type  string
 }
 
-func (l *LocationService) GetLocation(ctx context.Context, address string) (*maps.LatLng, error) {
-	if pos, ok := l.cache[address]; ok {
-		return pos, nil
+// Deprecated: use [Provider] instead.
+type LocationService = Provider //nolint: all
+
+type Provider struct {
+	client *maps.Client
+	cache  sync.Map
+}
+
+// GetLocation tries to the the latitude and longitude for the given address
+func (l *Provider) GetLocation(ctx context.Context, address string) (*LatLng, error) {
+	if pos, ok := l.cache.Load(address); ok {
+		return pos.(*LatLng), nil
 	}
 
+	// fetch the location using geocode request.
 	results, err := l.client.Geocode(ctx, &maps.GeocodingRequest{Address: address})
 	if err != nil {
 		return nil, err
 	}
 
+	// select the first exact match or partial match if no exact matches exist.
 	var result *maps.GeocodingResult
-	for i := range results {
-		if results[i].PartialMatch {
-			result = &results[i]
-		} else if result == nil {
-			result = &results[i]
+	for idx := range results {
+		if !results[idx].PartialMatch {
+			result = &results[idx]
+			break
+		}
+
+		if result == nil {
+			result = &results[idx]
 		}
 	}
 
+	// no matches
 	if result == nil {
 		return nil, ErrLocation
 	}
 
-	pos := &result.Geometry.Location
-	l.cache[address] = pos
+	pos := &LatLng{LatLng: result.Geometry.Location, Exact: !result.PartialMatch, Type: "point"}
+	l.cache.Store(address, pos)
 	return pos, nil
 }
 
-func New(apiKey string) (*LocationService, error) {
+// New creates a new location provider.
+// apiKey is the google maps api key to use.
+func New(apiKey string) (*Provider, error) {
 	c, err := maps.NewClient(maps.WithAPIKey(apiKey))
 	if err != nil {
 		return nil, err
 	}
 
-	return &LocationService{client: c, cache: map[string]*maps.LatLng{}}, nil
+	return &Provider{client: c}, nil
 }
