97d1948a15e071ff5bb93fed010c83c969ab973e
yehan2002 <yehanjaya2002@gmail.com>
2025-05-09 13:18:13 +0530

    shared: move auth to middleware/auth package.

--- shared/middleware/error.go
+++ shared/middleware/error.go
@@ -15,7 +15,14 @@ type validationError interface {
 
 // ErrorHandler handles errors returned by the server.
 // This handles logging the error and returning an error message to the client.
-func ErrorHandler(log *zap.Logger) fiber.ErrorHandler {
+func ErrorHandler(logger ...*zap.Logger) fiber.ErrorHandler {
+	var log *zap.Logger
+	if len(logger) > 0 {
+		log = logger[0]
+	} else {
+		log = zap.L()
+	}
+
 	// disable stacktrace for messages logged from this function.
 	// This is disabled because the stacktrace does not contain any information about the handler function.
 	log = log.WithOptions(zap.AddStacktrace(zap.PanicLevel))
@@ -26,17 +33,17 @@ func ErrorHandler(log *zap.Logger) fiber.ErrorHandler {
 			var fiberError *fiber.Error
 			if errors.As(err, &fiberError) {
 				log.Warn("Request returned error", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
-				return ctx.Status(fiberError.Code).JSON(dto.ErrorResponse{Ok: false, Error: fiberError.Message})
+				return ctx.Status(fiberError.Code).JSON(dto.Error(fiberError.Message))
 			}
 
 			// If the error is a validation error, send a bad request error with the validation error details
 			if verr, ok := err.(validationError); ok {
 				log.Warn("Invalid request from client", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
-				return ctx.Status(400).JSON(dto.ErrorResponse{Ok: false, Error: verr.Error(), Reason: verr.ValidationErrors()})
+				return ctx.Status(fiber.StatusBadRequest).JSON(dto.Error(verr.Error(), verr.ValidationErrors()))
 			}
 
 			log.Error("Error occurred while handling request", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
-			return ctx.Status(fiber.StatusInternalServerError).JSON(dto.ErrorResponse{Ok: false, Error: "An internal error occurred while handling the request"})
+			return ctx.Status(fiber.StatusInternalServerError).JSON(dto.Error("An internal error occurred while handling the request"))
 		}
 		return nil
 	}
