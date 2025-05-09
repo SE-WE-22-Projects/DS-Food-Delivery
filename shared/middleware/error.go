53c1c6b956400537772413195f644380d0625f94
yehan2002 <yehanjaya2002@gmail.com>
2025-05-09 14:24:43 +0530

    shared: fix log initialization bug.

--- shared/middleware/error.go
+++ shared/middleware/error.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"errors"
+	"sync"
 
 	"github.com/SE-WE-22-Projects/DS-Food-Delivery/shared/dto"
 	"github.com/gofiber/fiber/v3"
@@ -16,33 +17,35 @@ type validationError interface {
 // ErrorHandler handles errors returned by the server.
 // This handles logging the error and returning an error message to the client.
 func ErrorHandler(logger ...*zap.Logger) fiber.ErrorHandler {
-	var log *zap.Logger
-	if len(logger) > 0 {
-		log = logger[0]
-	} else {
-		log = zap.L()
-	}
+	getLogger := sync.OnceValue(func() *zap.Logger {
+		var log *zap.Logger
+		if len(logger) > 0 {
+			log = logger[0]
+		} else {
+			log = zap.L()
+		}
 
-	// disable stacktrace for messages logged from this function.
-	// This is disabled because the stacktrace does not contain any information about the handler function.
-	log = log.WithOptions(zap.AddStacktrace(zap.PanicLevel))
+		// disable stacktrace for messages logged from this function.
+		// This is disabled because the stacktrace does not contain any information about the handler function.
+		return log.WithOptions(zap.AddStacktrace(zap.PanicLevel))
+	})
 
 	return func(ctx fiber.Ctx, err error) error {
 		if err != nil {
 			// If the error is a fiber.Error, it is an api error that should be sent to the client.
 			var fiberError *fiber.Error
 			if errors.As(err, &fiberError) {
-				log.Warn("Request returned error", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
+				getLogger().Warn("Request returned error", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
 				return ctx.Status(fiberError.Code).JSON(dto.Error(fiberError.Message))
 			}
 
 			// If the error is a validation error, send a bad request error with the validation error details
 			if verr, ok := err.(validationError); ok {
-				log.Warn("Invalid request from client", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
+				getLogger().Warn("Invalid request from client", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
 				return ctx.Status(fiber.StatusBadRequest).JSON(dto.Error(verr.Error(), verr.ValidationErrors()))
 			}
 
-			log.Error("Error occurred while handling request", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
+			getLogger().Error("Error occurred while handling request", zap.Error(err), zap.String("path", string(ctx.Request().URI().Path())))
 			return ctx.Status(fiber.StatusInternalServerError).JSON(dto.Error("An internal error occurred while handling the request"))
 		}
 		return nil
