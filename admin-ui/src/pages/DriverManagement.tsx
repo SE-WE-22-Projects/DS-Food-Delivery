bd4ede126b48ed7c968e70ca885408f0e362285f
navindunimsara2001 <navidunimsara@gmail.com>
2025-05-17 17:05:01 +0530

    admin-ui: ui enhancements

--- admin-ui/src/pages/DriverManagement.tsx
+++ admin-ui/src/pages/DriverManagement.tsx
@@ -1,10 +1,34 @@
+
+const restaurantTabs: DynamicTabsProps = {
+    tabData: [
+        {
+            label: "All",
+            value: "all",
+            content: <AllRestaurantTab />
+        },
+        {
+            label: "Pending Restaurants",
+            value: "pending",
+            content: <PendingRestaurantTab />,
+        },
+        {
+            label: "Approved Restaurants",
+            value: "approved",
+            content: <ApprovedRestaurantTab />
+        }
+    ]
+}
+
 const DriverManagement = () => {
     return (
         <>
             {/* page title */}
-            <div className="flex justify-center w-full">
-                <h1 className="text-4xl lg:text-5xl">Driver Management</h1>
-            </div>
+            <header className="mb-6 md:mb-8">
+                <h1 className="text-3xl md:text-4xl font-bold pb-7 tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 via-cyan-700 to-blue-500 text-center">
+                    Driver Management
+                </h1>
+                <p className="text-slate-400 mt-1">Manage and view all restaurant details.</p>
+            </header>
         </>
     )
 }
