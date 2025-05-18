1f4bfca5fb3a7716e40f5a2a09800dca84865a11
yehan2002 <yehanjaya2002@gmail.com>
2025-05-18 12:01:42 +0530

    web-ui: api changes.

--- web-ui/src/lib/images.ts
+++ web-ui/src/lib/images.ts
@@ -1,10 +1,13 @@
 import NotFound from "@/assets/NotFound.svg";
 
 const getImageUrl = (image: string | undefined) => {
+    if (!image) return NotFound;
 
-    if (image && image.startsWith("/api/v1/")) {
+
+    if (image.startsWith("/api/v1/") || image.startsWith("data:")) {
         return image
     }
+
     return NotFound;
 }
 
