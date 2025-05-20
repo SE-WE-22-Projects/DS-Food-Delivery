33474127a4fc34928a980d583a4b4bcbc54c400e
yehan2002 <yehanjaya2002@gmail.com>
2025-05-20 23:05:20 +0530

    shared: add default middleware.

--- shared/server.go
+++ shared/server.go
@@ -8,6 +8,9 @@ import (
 	"github.com/SE-WE-22-Projects/DS-Food-Delivery/shared/middleware"
 	"github.com/SE-WE-22-Projects/DS-Food-Delivery/shared/validate"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/compress"
+	"github.com/gofiber/fiber/v3/middleware/healthcheck"
+	"github.com/gofiber/fiber/v3/middleware/limiter"
 )
 
 // DefaultFiberConfig is the default fiber config used for servers.
@@ -15,6 +18,16 @@ var DefaultFiberConfig = fiber.Config{
 	ErrorHandler:    middleware.ErrorHandler(),
 	JSONDecoder:     middleware.UnmarshalJsonStrict,
 	StructValidator: validate.New(),
+}
+
+// WithDefaultMiddleware registers default middleware for the server.
+func WithDefaultMiddleware(app *fiber.App) *fiber.App {
+	app.Use(middleware.Recover())
+	app.Use("/health/livez", healthcheck.NewHealthChecker())
+	app.Use(compress.New())
+	app.Use(limiter.New())
+
+	return app
 }
 
 func AppContext() context.Context {
